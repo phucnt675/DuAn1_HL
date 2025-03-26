2272627bd5a765515631301d8cc47fedb8ee47af
QuangHuy <139316919+QangQii@users.noreply.github.com>
2025-03-26 08:00:43 +0700

    [ Client ] - Update

--- App/Views/Client/Pages/Testimonial/Index.php
+++ App/Views/Client/Pages/Testimonial/Index.php
@@ -17,51 +17,6 @@ class Index extends BaseView
             <div class="main_body_content">
 
                 <div class="hero_area">
-                    <!-- header section strats -->
-                    <header class="header_section">
-                        <div class="container-fluid">
-                            <nav class="navbar navbar-expand-lg custom_nav-container ">
-                                <a class="navbar-brand" href="index.html">
-                                    ChocoLux
-                                </a>
-                                </a>
-                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class=""> </span>
-                                </button>
-
-                                <div class="collapse navbar-collapse " id="navbarSupportedContent">
-                                    <ul class="navbar-nav ml-auto">
-                                        <li class="nav-item ">
-                                            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="about.html"> About</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="chocolate.html">Chocolates</a>
-                                        </li>
-                                        <li class="nav-item active">
-                                            <a class="nav-link" href="testimonial.html">Testimonial</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="contact.html">Contact Us</a>
-                                        </li>
-                                    </ul>
-                                    <div class="quote_btn-container">
-                                        <form class="form-inline">
-                                            <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-                                                <i class="fa fa-search" aria-hidden="true"></i>
-                                            </button>
-                                        </form>
-                                        <a href="">
-                                            <i class="fa fa-user" aria-hidden="true"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </nav>
-                        </div>
-                    </header>
-                    <!-- end header section -->
                 </div>
 
 
